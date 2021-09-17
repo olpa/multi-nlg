f40df34e4517a2dd23cb494a480279cad4040015
Parashchenko Oleg <olpa@uucode.com>
2021-09-17 09:58:40 +0200

    Switch to DP.
    Squashed commit of the following:
    
    commit 65e46e88e4bf0961932ec40f5a7b6c9ad6b2d740
    Author: Parashchenko Oleg <olpa@uucode.com>
    Date:   Fri Sep 17 09:58:15 2021 +0200
    
        Lint.
    
    commit 58deb1a4c9cb01a56c4d10b61e10271e0be92234
    Author: Parashchenko Oleg <olpa@uucode.com>
    Date:   Fri Sep 17 09:49:16 2021 +0200
    
        Rework tanru translation.
    
    commit 6b9343639065acaa8f538c6e3510cf367d2a018a
    Author: Parashchenko Oleg <olpa@uucode.com>
    Date:   Fri Sep 17 08:49:41 2021 +0200
    
        Test to lcs personal name.
    
    commit 9e1edc98eb2bc4a213cdc126b21079726f18d31d
    Author: Parashchenko Oleg <olpa@uucode.com>
    Date:   Fri Sep 17 08:38:55 2021 +0200
    
        Update expected LCS.

--- mnlg/camxes_to_lcs.py
+++ mnlg/camxes_to_lcs.py
@@ -1,8 +1,9 @@
-# From camxes parse tree to a LCS (lexical conceptual structure)
+import sys
+
 from mnlg.transform import MatchNameCondition, Rule, TransformChildren,\
     MatchName, TreeNode, NodeSet, select, SelectStep, DeepDive,\
     apply_templates, Transformer, Replace, apply_templates_iter,\
-    project_children, Drop, MatchAlways, flatten_node_sets,\
+    project_children, Drop, flatten_node_sets,\
     TransformRename, Matcher
 
 
@@ -56,31 +57,49 @@ class TransformSentence(Transformer):
                            ]]]
 
 
+def is_node_name(node: TreeNode, name: str) -> bool:
+    if not isinstance(node, list):
+        return False
+    if not node:
+        return False
+    return node[0] == name
+
+
 class TransformSumti(Transformer):
     def transform(self, rules: list['Rule'], node: TreeNode) -> NodeSet:
-        spec = apply_templates(rules, node[1:2])
-        bar = apply_templates(rules, node[2:])
-        if len(spec) == 1 and next(iter(spec[0]), '') == 'N':  # KOhA
-            bar = [*spec, *bar]
-            spec = []
-        if len(spec) == 1 and len(spec[0]) == 1:  # 'le'
-            spec = spec[0]
-        node = ['N-MAX']
-        if spec:
-            node.append(['N-SPEC', *spec])
-        if bar:
-            node.append(['N-BAR', *bar])
-        return [node]
+        kids = apply_templates(rules, node[1:])
+        if not kids:
+            print('TransformSumti: no kids after transformation',
+                  file=sys.stderr)
+            return []
+        kids = kids[:2]
+
+        nbar = kids.pop()
+        if not is_node_name(nbar, 'N-BAR'):
+            print('TransformSumti: after transform,',
+                  'the last kid should be N-BAR, got:',
+                  nbar, file=sys.stderr)
+            return []
+        nmax = ['N-MAX', nbar]
+
+        dmax = None
+        if kids:
+            dbar = kids[0]
+            if is_node_name(dbar, 'D-BAR'):
+                dbar.append(nmax)
+                dmax = ['D-MAX', dbar]
+            else:
+                print('TransformSumti: after transform,',
+                      'the first kid should be D-BAR, got:',
+                      kids[0], file=sys.stderr)
+
+        return [dmax or nmax]
 
 
-class TransformCmevlaClause(Transformer):
+class TransformCmevla(Transformer):
     def transform(self, rules: list['Rule'], node: TreeNode) -> NodeSet:
-        cmevla = select(node, [
-            MatchAlways(), MatchName('CMEVLA'),
-            MatchName('cmevla'), MatchAlways()
-        ])
-        cmevla = next(iter(cmevla), '???')
-        return [['N', cmevla]]
+        word = next(iter(project_children(node)), '???')
+        return [['N', ['tag', 'pn'], word]]
 
 
 class TransformWord(Transformer):
@@ -92,7 +111,7 @@ class TransformWord(Transformer):
 class TransformKoha(Transformer):
     def transform(self, rules: list['Rule'], node: TreeNode) -> NodeSet:
         word = next(iter(project_children(node)), '???')
-        return [['N', word]]
+        return [['N', ['tag', 'pron'], word]]
 
 
 def camxes_to_lcs(tree) -> list:
@@ -110,15 +129,19 @@ def camxes_to_lcs(tree) -> list:
     skip_time = Rule(match_name_begin('time'), TransformChildren())
     rule_pu = Rule(MatchName('PU_clause'), Replace([['tag', 'pu']]))
     rule_sumti = Rule(MatchName('sumti_6'), TransformSumti())
+    rule_sumti_nbar = Rule(MatchName('sumti_tail'), TransformRename('N-BAR'))
     skip_sumti = Rule(match_name_begin('sumti'), TransformChildren())
-    rule_la = Rule(MatchName('LA_clause'), Drop())
+    rule_la = Rule(MatchName('LA_clause'), TransformChildren())
     drop_la = Rule(MatchName('LA'), Drop())
-    rule_le = Rule(MatchName('LE_clause'), Replace([['le']]))
-    drop_le = Rule(MatchName('LE'), Drop())
+    rule_le = Rule(MatchName('LE_clause'), TransformRename('D-BAR'))
+    drop_le = Rule(MatchName('LE'), Replace([['D', ['tag', 'le']]]))
     drop_ku = Rule(MatchName('KU'), Drop())
-    skip_koha = Rule(MatchName('KOhA_clause'), TransformChildren())
+    skip_koha = Rule(MatchName('KOhA_clause'), TransformRename('N-BAR'))
     rule_koha = Rule(MatchName('KOhA'), TransformKoha())
-    rule_smevla = Rule(MatchName('CMEVLA_clause'), TransformCmevlaClause())
+    rule_smevla = Rule(MatchName('cmevla'), TransformCmevla())
+    rule_smevla_wrapper = Rule(MatchName('CMEVLA'), TransformChildren())
+    rule_smevla_clause = Rule(MatchName('CMEVLA_clause'),
+                              TransformRename('N-BAR'))
     rule_brivla = Rule(MatchName('BRIVLA'), TransformRename('N'))
     skip_brivla = Rule(match_name_begin('BRIVLA'), TransformChildren())
     skip_tanru_unit = Rule(match_name_begin('tanru_unit'), TransformChildren())
@@ -130,10 +153,11 @@ def camxes_to_lcs(tree) -> list:
         skip_text, skip_paragraph, skip_statement, rule_sentence,
         skip_tag, skip_tense_modal, skip_simple_tense_modal,
         skip_time, rule_pu,
-        rule_sumti, skip_sumti, rule_la, drop_la, rule_le,
+        rule_sumti, rule_sumti_nbar, skip_sumti, rule_la, drop_la, rule_le,
         drop_le, drop_ku,
         rule_brivla, skip_brivla,
-        rule_smevla, rule_lujvo, rule_gismu,
+        rule_smevla, rule_smevla_wrapper, rule_smevla_clause,
+        rule_lujvo, rule_gismu,
         skip_tanru_unit, skip_selbri,
         skip_koha, rule_koha,
     ], tree)
@@ -143,7 +167,6 @@ def camxes_to_lcs(tree) -> list:
 
 if '__main__' == __name__:
     import json
-    import sys
     camxes_tree = json.load(sys.stdin)
     lcs_tree = camxes_to_lcs(camxes_tree)
     print(lcs_tree)
--- tests/camxes_to_lcs_test.py
+++ tests/camxes_to_lcs_test.py
@@ -6,6 +6,43 @@ from mnlg.camxes_to_lcs import camxes_to_lcs
 
 
 class CamxesToLcsTest(unittest.TestCase):
+
+    @staticmethod
+    def test_personal_name():
+        tree = ['sumti_6', ['LA_clause', [['LA', 'la']]],
+                [['CMEVLA_clause', [['CMEVLA', ['cmevla', 'djan']]]]]]
+
+        lcs = camxes_to_lcs(tree)
+
+        assert_that(lcs, equal_to(
+            ['N-MAX', ['N-BAR', ['N', ['tag', 'pn'], 'djan']]]))
+
+    @staticmethod
+    def test_pronoun():
+        tree = ["sumti_6", ["KOhA_clause", [["KOhA", "mi"]]]]
+
+        lcs = camxes_to_lcs(tree)
+
+        assert_that(lcs, equal_to(
+            ['N-MAX', ['N-BAR', ['N', ['tag', 'pron'], 'mi']]]))
+
+    @staticmethod
+    def test_le():
+        tree = ["sumti_6", ["LE_clause", [["LE", "le"]]], ["sumti_tail",
+                ["sumti_tail_1", ["selbri", ["selbri_1", ["selbri_2",
+                 ["selbri_3", ["selbri_4", ["selbri_5", ["selbri_6",
+                  ["tanru_unit", ["tanru_unit_1", ["tanru_unit_2",
+                   ["BRIVLA_clause", [["BRIVLA",
+                    ["gismu", "kumfa"]]]]]]]]]]]]]]]], ["KU"]]
+
+        lcs = camxes_to_lcs(tree)
+
+        assert_that(lcs, equal_to(
+            ['D-MAX', ['D-BAR', ['D', ['tag', 'le']],
+                       ['N-MAX', ['N-BAR', ['N', 'kumfa']]]]]))
+
+
+class CamxesToLcsExamplesTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.trees = load_camxes_parses()
