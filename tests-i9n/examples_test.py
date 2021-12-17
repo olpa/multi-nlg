83283224d9dd7bda377cce9356a9e04856e3293b
Parashchenko Oleg <olpa@uucode.com>
2021-12-17 13:36:19 +0100

    Add integration test for "mi prami do".

--- tests-i9n/examples_test.py
+++ tests-i9n/examples_test.py
@@ -87,6 +87,18 @@ class Example(unittest.TestCase):
         }
         assert_example(self.mnlg, s_in, expected_translations)
 
+    def test_love(self):
+        s_in = 'mi prami do'
+        expected_translations = {
+                'de': 'Ich liebe dich.',
+                'en': 'I stabbed John.',
+                'es': 'Ti amo.',
+                'ru': 'Я люблю тебя.',
+                'zh': '我愛你。',
+                }
+
+        assert_example(self.mnlg, s_in, expected_translations)
+
 
 if '__main__' == __name__:
     unittest.main()
